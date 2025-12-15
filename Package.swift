024f4f10779e843c9ee68c05243c0060dc482103
Magzhan <mimangazy@beeline.kz>
2025-12-15 11:00:43 +0500

    add support simulator architecture for xcframework

--- Package.swift
+++ Package.swift
@@ -18,8 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TCMPPExtMiniGame",
-            url: "https://tmf-warehouse-1314481471.cos.ap-beijing.myqcloud.com/tcmpp/ios-sdk/noshark/TCMPPExtMiniGame/2.2.3/17/TCMPPExtMiniGame_2.2.3.xcframework.zip",
-            checksum: "705e195360af55f8c1975d40e2d718d3c2c63572b6f675a4f9d76a260a74c90c"
+            path: "Sources/TCMPPExtMiniGame.xcframework"
         ),
         .target(
             name: "TCMPPExtMiniGameWrapper",
