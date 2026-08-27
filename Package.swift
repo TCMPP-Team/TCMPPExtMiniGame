c3601b99c725b2158ffa486a65c9bfa8fae36ea4
TCMPP-Team <tcmpp_superapp@tencent.com>
2026-08-27 10:42:08 +0800

    Add linker settings for TCMPPExtMiniGameWrapper

--- Package.swift
+++ Package.swift
@@ -26,7 +26,8 @@ let package = Package(
             dependencies: [
                 "TCMPPExtMiniGame",
             ],
-            resources: [.copy("magicbrushres.bundle")]
+            resources: [.copy("magicbrushres.bundle")],
+            linkerSettings: [ .linkedLibrary("c++") ]
         )
     ]
 ) 
